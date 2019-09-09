e5226add3212773b7723a3fee27ef1eb5def1a48
Jackson-zhanganbing <635590867@qq.com>
2019-09-09 21:17:52 +0800

    spring cloud bus

--- config-bus/src/test/java/com/changan/configbus/ConfigBusApplicationTests.java
+++ config-bus/src/test/java/com/changan/configbus/ConfigBusApplicationTests.java
@@ -1,4 +1,4 @@
-package com.changan.configclientgit;
+package com.changan.configbus;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ConfigClientGitApplicationTests {
+public class ConfigBusApplicationTests {
 
     @Test
     public void contextLoads() {
