d75961657ee3c4316c1f05c358de965bb5f06218
zab <635590867@qq.com>
2021-05-26 10:24:57 +0800

    gateway模块更名zuuldemo

--- zuuldemo/src/main/java/com/changan/apigateway/ApiGatewayApplication.java
+++ zuuldemo/src/main/java/com/changan/apigateway/ApiGatewayApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * 使用说明：在本服务端口后面加上其他微服务名称就可以路由到
+ * http://localhost:14000/microservice-consumer-user/api/user
+ */
 @SpringBootApplication
 @EnableZuulProxy
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
 }
