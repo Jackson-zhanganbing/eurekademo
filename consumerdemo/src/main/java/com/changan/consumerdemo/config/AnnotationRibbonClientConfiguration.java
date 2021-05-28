155d64f3a05964236f69195213768924cf7cb3ee
zab <635590867@qq.com>
2021-05-28 13:47:57 +0800

    自定义算法获得服务列表

--- consumerdemo/src/main/java/com/changan/consumerdemo/config/AnnotationRibbonClientConfiguration.java
+++ consumerdemo/src/main/java/com/changan/consumerdemo/config/AnnotationRibbonClientConfiguration.java
@@ -1,28 +1,19 @@
 package com.changan.consumerdemo.config;
 
 import com.netflix.client.config.IClientConfig;
-import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RandomRule;
-import com.netflix.loadbalancer.RoundRobinRule;
+import com.netflix.loadbalancer.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
+
 /**
  * 这里不使用@Configuration配置，防止称为全局配置
+ * 可以继承AbstractLoadBalancerRule获得所有服务列表，自定义算法
  *
  * @author zab
- * @date 2019-08-19 20:49
+ * @date 2021-5-28 13:47:08
  */
 public class AnnotationRibbonClientConfiguration {
-/*    @Bean
-    public ServerList<Server> ribbonServerList() {
-        Server[] servers = new Server[2];
-        servers[0] = new Server("http://localhost:8001");
-        servers[1] = new Server("http://localhost:8002");
-        ServerList<Server> serverList = new StaticServerList<>(servers);
-        return serverList;
-    }
-*/
 
     @Autowired
     private IClientConfig config;
@@ -39,12 +30,7 @@ public class AnnotationRibbonClientConfiguration {
         //return new RandomRule();
     }
 
-
- /*   @Override
-    public void initWithNiwsConfig(IClientConfig iClientConfig) {
-
-    }
-
+/*
     @Override
     public Server choose(Object o) {
         ILoadBalancer lb = this.getLoadBalancer();
@@ -52,6 +38,16 @@ public class AnnotationRibbonClientConfiguration {
         List<Server> upServerList = lb.getReachableServers();
         List<Server> allServerList = lb.getAllServers();
 
-        return null;
+        int size = allServerList.size();
+
+        Random random = new Random(10);
+        int i = random.nextInt(size);
+        return allServerList.get(i - 1);
+    }
+
+    @Override
+    public void initWithNiwsConfig(IClientConfig iClientConfig) {
+
     }*/
+
 }
